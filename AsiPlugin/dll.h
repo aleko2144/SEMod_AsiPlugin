05de13257bda3e642b4b35b9bd45e6b7eac22a55
aleko2144 <42273133+aleko2144@users.noreply.github.com>
2019-12-08 17:59:07 +0300

    Add files via upload

---
+++ AsiPlugin/dll.h
@@ -0,0 +1,19 @@
+#ifndef _DLL_H_
+#define _DLL_H_
+#define BUILDING_DLL 1
+
+#if BUILDING_DLL
+#define DLLIMPORT __declspec(dllexport)
+#else
+#define DLLIMPORT __declspec(dllimport)
+#endif
+
+class DLLIMPORT DllClass
+{
+  public:
+	DllClass();
+	virtual ~DllClass();
+	void HelloWorld();
+};
+
+#endif
