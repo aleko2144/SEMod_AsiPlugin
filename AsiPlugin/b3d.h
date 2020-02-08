0fb26620898f121a66a256e1c97b27e689a349f8
aleko2144 <42273133+aleko2144@users.noreply.github.com>
2020-02-08 21:38:25 +0300

    Update b3d.h

--- AsiPlugin/b3d.h
+++ AsiPlugin/b3d.h
@@ -6,9 +6,9 @@ namespace b3d
 	void SetCaseSwitch(int *object, int state);
 	int *FindGameObject(int *parent, const char *name);
 	int *SearchBlock(int *parent, char *name, char *a3);
+	int GetChildCount(int *offset);
 	void SetCaseSwitch_s(int *offset, int state);
 	int GetCaseSwitch(int *offset);
-	int GetChildCount(int *offset);
 }
 
 #endif
