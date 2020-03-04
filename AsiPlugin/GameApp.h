8f78b9861fec77822f026e007837bd389d3a998a
aleko2144 <42273133+aleko2144@users.noreply.github.com>
2020-03-04 19:25:24 +0300

    Update GameApp.h

--- AsiPlugin/GameApp.h
+++ AsiPlugin/GameApp.h
@@ -8,7 +8,7 @@ namespace GameApp
 	void DisplayScreenMessage(char* text);
 	void DisplayPagerMessage(char* text);
 	int SearchResourceSND(char *a1);
-	void PlaySound_(int soundFile, float a2, float a3);
+	void PlaySound_(int soundFile, float right_channel_volume, float left_channel_volume);
 	void PlaySoundLocated(int soundFile, float a2, float a3, Vector3D *Position);
 	int GetPlayerID();
 	double GetPlayerCapital(int playerID);
